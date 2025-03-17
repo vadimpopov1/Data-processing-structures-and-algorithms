992a69f073fa3c6644a70a2f3f076950212bdee3
vadimpopov1 <bonia07052006@icloud.com>
2025-03-17 11:17:29 +0700

    Update 3.c

--- lab7/3.c
+++ lab7/3.c
@@ -19,58 +19,70 @@ void shellSort(int arr[], int n) {
     }
 }
 
-int binarySearchIterative(int arr[], int n, int key, int *comparisons) {
-    int low = 0, high = n - 1;
-    while (low <= high) {
+int binarySearch(int array[], int size, int target, int *comparisons) {
+    int left = 0;
+    int right = size - 1;
+    
+    while (left <= right) {
+        int mid = (left + right) / 2;
         (*comparisons)++;
-        int mid = low + (high - low) / 2;
-        if (arr[mid] == key)
+        if (array[mid] == target) {
             return mid;
-        if (arr[mid] < key)
-            low = mid + 1;
-        else
-            high = mid - 1;
+        }
+        (*comparisons)++;
+        if (array[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
     return -1;
 }
 
-int binarySearchRecursive(int arr[], int low, int high, int key, int *comparisons) {
-    if (high >= low) {
+int binarySearchUpper(int array[], int size, int target, int *comparisons) {
+    int left = 0;
+    int right = size - 1;
+    
+    while (left < right) {
+        int mid = left + (right - left) / 2;
         (*comparisons)++;
-        int mid = low + (high - low) / 2;
-        if (arr[mid] == key)
-            return mid;
-        if (arr[mid] > key)
-            return binarySearchRecursive(arr, low, mid - 1, key, comparisons);
-        return binarySearchRecursive(arr, mid + 1, high, key, comparisons);
+        if (array[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
     }
-    return -1;
+    (*comparisons)++;
+    if (left < size && array[left] == target) {
+        return left;
+    } else {
+        return -1;
+    }
 }
 
-void FillRand(int n, int A[]) {
+void fillRandom(int n, int array[]) {
     for (int i = 0; i < n; i++) {
-        A[i] = rand() % 1000; 
+        array[i] = rand() % 1000; 
     }
 }
 
 int main() {
-
     int sizes[] = {100, 500, 1000};
-    int num_sizes = sizeof(sizes) / sizeof(sizes[0]);
+    int numSizes = sizeof(sizes) / sizeof(sizes[0]);
 
-    printf("| N\t| Сф I\t| Сф II\t|\n");
+    printf("| N\t| C1\t| C2\t|\n");
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < numSizes; i++) {
         int n = sizes[i];
         int arr[n];
         srand(time(NULL));
-        FillRand(n, arr);
+        fillRandom(n, arr);
         shellSort(arr, n);
         int m = rand() % n;
         int key = arr[m];
         int comparisons1 = 0, comparisons2 = 0;
-        binarySearchIterative(arr, n, key, &comparisons1);
-        binarySearchRecursive(arr, 0, n - 1, key, &comparisons2);
+        binarySearch(arr, n, key, &comparisons1);
+        binarySearchUpper(arr, n, key, &comparisons2);
         printf("| %d\t| %d\t| %d\t|\n", n, comparisons1, comparisons2);
     }
 
